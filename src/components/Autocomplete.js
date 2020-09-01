d88c30ad83439ec4c23c1a7a9e22b52db7714e44
Zeb Engberg <zebengberg@gmail.com>
2020-08-31 22:16:30 -0600

    implemented logic for preferences component

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Autocomplete from "react-autocomplete";
 
 export default (props) => (
--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -1,16 +1,37 @@
 import React, { useState } from "react";
-import { Container, Row, Button } from "react-bootstrap";
+import { Container, Row, Button, Alert } from "react-bootstrap";
+import Link from "react-router-dom/Link";
 import RankedRoute from "./RankedRoute";
 
 export default () => {
   const [routeList, setRouteList] = useState([{ route: "", rating: 0 }]);
-  console.log(routeList);
+  const [buttonClicked, setButtonClicked] = useState(false);
+
+  // Add a new <RankedRoute> if the last one has been filled.
+  // Reset the warning alert
   if (
     routeList[routeList.length - 1].route &&
     routeList[routeList.length - 1].rating
   ) {
     setRouteList([...routeList, { route: "", rating: 0 }]);
+    setButtonClicked(false);
   }
+
+  // Determine if sufficient route preferences are given for submission.
+  const minDistinctRoutesRequired = 5;
+
+  // Helper function
+  const getValidAndDistinctEntries = (list) => {
+    const validList = list.filter((o) => o.route && o.rating);
+    // Using filter for routeNames to avoid empty string as name
+    const routeNames = validList.map((o) => o.route).filter((o) => o);
+    const firstIndices = routeNames.map((name) => routeNames.indexOf(name));
+    const firstIndicesNoDuplicates = [...new Set(firstIndices)];
+    return firstIndicesNoDuplicates.map((i) => validList[i]);
+  };
+
+  const ConditionalLink = ({ children, to, condition }) =>
+    condition && to ? <Link to={to}>{children}</Link> : <>{children}</>;
 
   return (
     <Container className="p-3">
@@ -20,7 +41,6 @@ export default () => {
         of your current preferences. Enter several Rumney routes for which you
         have strong opinions (positive or negative) below.
       </p>
-
       <Row>
         {routeList.map((routeObject, index) => (
           <RankedRoute
@@ -41,8 +61,27 @@ export default () => {
           />
         ))}
       </Row>
-
-      <Button>Get recommendation</Button>
+      <ConditionalLink
+        to="/results"
+        condition={
+          getValidAndDistinctEntries(routeList).length >
+          minDistinctRoutesRequired
+        }
+      >
+        <Button onClick={(e) => setButtonClicked(true)}>
+          Get Recommendation
+        </Button>
+      </ConditionalLink>
+      {getValidAndDistinctEntries(routeList).length <
+        minDistinctRoutesRequired &&
+        buttonClicked && (
+          <Alert variant="warning">
+            You must complete preferences for {minDistinctRoutesRequired}{" "}
+            distinct routes to continue. So far you have only completed
+            preferences for {getValidAndDistinctEntries(routeList).length}{" "}
+            distinct routes.
+          </Alert>
+        )}
     </Container>
   );
 };

050d802377941279a6e375ca012b8805aec2cd65
Zeb Engberg <zebengberg@gmail.com>
2020-10-13 07:31:09 -0600

    include method for scraping route description and comments; fix rating scrap to distinguish bomb from stars

--- backend/src/scrape_rumney_routes.py
+++ backend/src/scrape_rumney_routes.py
@@ -3,6 +3,7 @@
 from os import path
 import logging
 from time import sleep
+import json
 import requests
 import pandas as pd
 from lxml import html
@@ -11,18 +12,17 @@ from tqdm import tqdm
 
 URLS_PATH = '../data/rumney_urls.csv'
 DATA_PATH = '../data/rumney_data.csv'
+METADATA_PATH = '../data/rumney_metadata.json'
 LOG_PATH = '../data/scrape_history.log'
 
-logger = logging.getLogger(__name__)
 logging.basicConfig(filename=LOG_PATH,
-                    level=logging.INFO,
+                    level=logging.DEBUG,
                     format='%(asctime)s %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S')
 
 
 def download_route_urls():
   """Download CSV of Rumney route URLs from MP server."""
-
   # use local cached csv if one exists
   if path.exists(URLS_PATH):
     print('Using cached version of URL data.')
@@ -43,12 +43,12 @@ def download_route_urls():
 
     # counting total number of lines in downloaded CSV and logging count
     num_lines = sum(1 for line in open(URLS_PATH))
-    logging.info('Downloaded route URL csv with %s lines from MP.', num_lines)
-    logging.info('Data written to: %s', URLS_PATH.split('/')[-1])
+    logging.debug('Downloaded route URL csv with %s lines from MP.', num_lines)
+    logging.debug('Data written to: %s', URLS_PATH.split('/')[-1])
 
 
-def scrape_url(url):
-  """Find MP user star ratings for given route by searching the HTML tree."""
+def scrape_stats_url(url):
+  """Scrape stats version of MP page for user star ratings."""
   r = requests.get(url)
   tree = html.fromstring(r.content)
 
@@ -57,42 +57,105 @@ def scrape_url(url):
 
   # getting users and stars based on HTML structure
   users = []
-  stars = []
-  for user, star in table.getchildren():  # user, star are <td> elements
-    users.append(user.getchildren()[0].text)  # text of an <a> element
-    stars.append(len(star.getchildren()[1].getchildren()))  # counting <img>
+  ratings = []
+  for td1, td2 in table.getchildren():
+    users.append(td1.getchildren()[0].text)  # text of an <a> element
 
-  return users, stars
+    # dealing with star and bomb images
+    children = td2.getchildren()[1].getchildren()
+    if children[0].get('src') == '/img/stars/bombBlue.svg':
+      ratings.append(0)  # bombs get a rating of 0
+    else:  # img_type == '/img/stars/starBlue.svg'
+      ratings.append(len(children))
+
+  return users, ratings
 
 
-def build_dataframe():
-  """Save joining table in which each ordered pair (route, user) is a row."""
+def scrape_printer_url(url):
+  """Scrape printer-friendly version of MP page for description and comment text."""
+  r = requests.get(url)
+  tree = html.fromstring(r.content)
 
-  url_df = pd.read_csv(URLS_PATH)
-  url_df = url_df[url_df['Avg Stars'] != -1]  # ignoring routes without stars
-  routes = list(url_df['Route'])
-  urls = list(url_df['URL'])
-  ratings = list(url_df['Rating'])
+  # searching for a div with class=fr-view and taking the first one found
+  description = tree.xpath('//div[@class="fr-view"]')[0]
+  # getting all inner text and ignoring <br> elements
+  description = description.text_content()
+
+  # searching for comments
+  comments = tree.xpath('//div[@class="comment-body"]')
+  # grabbing the first child of the comment div
+  comments = [c.getchildren()[0] for c in comments]
+  # the comment itself is the tail text of the first child
+  comments = [c.tail for c in comments]
+
+  # concatenating all text together and returning massive string
+  text = [description] + comments
+  return ' '.join(text), len(comments)
+
+
+def process_url(url):
+  """Return stat and printer-friendly versions of URL of MP page."""
   url_prefix = 'https://www.mountainproject.com/route/'
   # inserting 'stats/' into url to get MP stats for each route
-  stat_urls = [url_prefix + 'stats/' + url[len(url_prefix):] for url in urls]
+  stats_url = url_prefix + 'stats/' + url[len(url_prefix):]
+  printer_url = url + '?print=1'
+  return stats_url, printer_url
 
+
+def read_url_csv():
+  """Read CSV of MP URLs and remove routes without any ratings."""
+  url_df = pd.read_csv(URLS_PATH)
+  url_df = url_df[url_df['Avg Stars'] != -1]  # ignoring routes without ratings
+  routes = list(url_df['Route'])
+  urls = list(url_df['URL'])
+  grades = list(url_df['Rating'])
+  return {'routes': routes, 'urls': urls, 'grades': grades}
+
+
+def build_ratings_dataframe():
+  """Save joining table in which each ordered pair (route, user) is a row."""
+  print('Scraping route-user-rating data.')
+
+  data = read_url_csv()
   rows = []
-  for route, url, stat_url, rating in zip(tqdm(routes), urls, stat_urls, ratings):
-    users, stars = scrape_url(stat_url)
+  for route, url in zip(tqdm(data['routes']), data['urls']):
+    stats_url, _ = process_url(url)
+    users, ratings = scrape_stats_url(stats_url)
     sleep(1)  # giving MP server a break
-    for user, star in zip(users, stars):
-      rows.append({'route': route, 'user': user, 'star': star,
-                   'grade': rating, 'url': url})
 
-  logging.info('Scraped %s route-user-star records from MP.', len(rows))
+    for user, rating in zip(users, ratings):
+      rows.append({'route': route, 'user': user, 'rating': rating})
 
+  logging.debug('Scraped %s route-user-rating records from MP.', len(rows))
   df = pd.DataFrame(rows)
   df.to_csv(DATA_PATH, index=False)
-  logging.info('Data written to: %s \n\n', DATA_PATH.split('/')[-1])
-  logging.shutdown()
+  logging.debug('Data written to: %s \n\n', DATA_PATH.split('/')[-1])
+
+
+def build_metadata_json():
+  """Save json file holding URL, grade, and text data for each route."""
+  print('Scraping route descriptions and comments.')
+
+  data = read_url_csv()
+  d = {}
+  total_n_comments = 0
+  for route, url, grade in zip(tqdm(data['routes']), data['urls'], data['grades']):
+    _, printer_url = process_url(url)
+    text, n_comments = scrape_printer_url(printer_url)
+    total_n_comments += n_comments
+    sleep(1)  # giving MP server a break
+
+    # append gathered data to dict
+    d[route] = {'url': url, 'grade': grade, 'text': text}
+
+  logging.debug('Scraped %s comments from MP.', total_n_comments)
+  with open(METADATA_PATH, 'w') as f:
+    json.dump(d, f)
+  logging.debug('Metadata written to: %s \n\n', METADATA_PATH.split('/')[-1])
 
 
 if __name__ == '__main__':
   download_route_urls()
-  build_dataframe()
+  build_ratings_dataframe()
+  # build_metadata_json()
+  logging.shutdown()
